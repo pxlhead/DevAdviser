00c4713e089576de8d51590b3a9074211b5951c9
Liza Kobrazova <lizakobrazova@gmail.com>
2017-10-09 11:28:30 +0300

    Import test data separately

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,7 @@
           span.team pxlhead tm
           .loader(v-if='loading')
             span.loader-title Loading
-            span.loader-bar [#############]
-            span.loader-percent [0%]
+            span.loader-bar {{ loaderBar }}
         .test(v-if='phase == 1')
           h3.test-question {{ activeTest.question }}
           .test-answers
@@ -21,11 +20,13 @@
           .loader(v-if='loading')
             span.loader-title {{ loaderText }}
           .result-message(v-else)
-            h3.result-text We suggest you to try &#60;{{ result }}&#62;
-            span.result-notification Press enter to restart
+            h3.result-text Try not. Do {{ result }} – or do not {{ result }}. There is no try.
+            span.result-notification Enter press to restart and be mindful.
 </template>
 
 <script>
+import testData from './testData'
+
 export default {
   name: 'app',
 
@@ -33,293 +34,14 @@ export default {
     return {
       phase: 0,
       loading: false,
+      loaderBar: '',
       loaderPhrases: [
         'Collecting your answers...',
         'Comparing answers with PL matrix...',
         'Testing your patience...',
         'Analizing...'
       ],
-      activePhrase: -1,
-
-      test: {
-        question: 'What is your purpose of studying?',
-        answers: [
-          'Web development',
-          'Software development',
-          'Mobile app development'
-        ],
-        '0': {
-          question: 'Do you like interacting with people?',
-          answers: [
-            'I\'m good with people',
-            'I hate them all'
-          ],
-          '0': {
-            question: 'Are you a creative person?',
-            answers: [
-              'I\'m a man of beauty',
-              'Functionality is more important for me'
-            ],
-            '0': 'HTML + CSS',
-            '1': 'JS'
-          },
-          '1': {
-            question: 'Are you a workaholic?',
-            answers: [
-              'Personal life is more important for me',
-              'Work is my credo'
-            ],
-            '0': {
-              question: 'Are you conservative or liberal?',
-              answers: [
-                'I love sorting out retro things',
-                'I\'m futurist'
-              ],
-              '0': 'PHP',
-              '1': 'Node.js'
-            },
-            '1': {
-              question: 'What kind kind of music do you prefer?',
-              answers: [
-                'Retrowave',
-                'Power metal',
-                'Alternative',
-                'Pop'
-              ],
-              '0': 'C#',
-              '1': 'Java',
-              '2': 'Ruby',
-              '3': 'Python'
-            }
-          }
-        },
-        '1': {
-          question: 'How do you usually spend your free time?',
-          answers: [
-            'Gaming!',
-            'Reinstalling OS',
-            'Thinking of business ideas',
-            'Building my own C-3PO',
-            'Doing my homework'
-          ],
-          '0': {
-            question: 'Which type of game you play last time?',
-            answers: [
-              'Indie',
-              'AAA',
-              'Text',
-              'MMORPG'
-            ],
-            '0': {
-              question: 'Which styles of visual art do you prefer?',
-              answers: [
-                'Realism',
-                'Surrealism',
-                'Cubism',
-                'Post-modernism',
-                'Avant-garde',
-                'Primitivism'
-              ],
-              '0': 'C++',
-              '1': 'C#',
-              '2': 'Java',
-              '3': 'Python',
-              '4': 'JS',
-              '5': 'Delphi'
-            },
-            '1': {
-              question: 'What is you favourite weapon in games?',
-              answers: [
-                'Bow to shot at someone\'s knee',
-                'Magic/Biotics/Psyonics',
-                'Knife (no matter what game it is)'
-              ],
-              '0': 'C++',
-              '1': 'C#',
-              '2': 'Java'
-            },
-            '2': {
-              question: 'Which type of literature whould you choose?',
-              answers: [
-                'Fantasy',
-                'Sci-fi',
-                'Manual/Docs/Guide',
-                'Documentary',
-                'Classic',
-                'Novel',
-                'Comics (OMG)'
-              ],
-              '0': 'C++',
-              '1': 'C#',
-              '2': 'Java',
-              '3': 'Python',
-              '4': 'Delphi',
-              '5': 'JS',
-              '6': 'ActionScript'
-            },
-            '3': {
-              question: 'Which fictional race would you like to be?',
-              answers: [
-                'Hafling',
-                'Dwarf',
-                'Klingon',
-                'Borg',
-                'Wookiee',
-                'Azari',
-                'Troll'
-              ],
-              '0': 'C++',
-              '1': 'C#',
-              '2': 'Java',
-              '3': 'Python',
-              '4': 'Delphi',
-              '5': 'JS',
-              '6': 'ActionScript'
-            }
-          },
-          '1': {
-            question: 'Which OS do you reinstall?',
-            answers: [
-              'Windows 😩',
-              'Linux (reboot now)',
-              'MacOS'
-            ],
-            '0': {
-              question: 'Which character would you be in the world of LOTR?',
-              answers: [
-                'J. R. R. Tolkien',
-                'Sauron',
-                'Gandalf',
-                'What is LOTR?',
-                'Treebeard',
-                'Dumbledore'
-              ],
-              '0': 'C',
-              '1': 'C++',
-              '2': 'Java',
-              '3': 'Rust',
-              '4': 'Delphi',
-              '5': 'Visual Basic'
-            },
-            '1': {
-              question: 'Which fiction film do you like?',
-              answers: [
-                'Star Trek (old)',
-                'Star Trek (new)',
-                'Star Wars',
-                'Men in black',
-                'Space Odyssey',
-                'Interstellar'
-              ],
-              '0': 'C',
-              '1': 'C++',
-              '2': 'Java',
-              '3': 'Python',
-              '4': 'Delphi',
-              '5': 'Go'
-            },
-            '2': {
-              question: 'Where would you spend your holidays?',
-              answers: [
-                'Home',
-                'Parent\'s home',
-                'Friend\'s home',
-                'Silicon Valley',
-                'Holidays?'
-              ],
-              '0': 'C',
-              '1': 'C++',
-              '2': 'Objective-C',
-              '3': 'Swift',
-              '4': 'Java'
-            }
-          },
-          '2': {
-            question: 'Which way would you choose to conquer the world?',
-            answers: [
-              'Political',
-              'Economical',
-              'Social',
-              'Military'
-            ],
-            '0': 'C++',
-            '1': 'Java',
-            '2': 'Python',
-            '3': 'Ada'
-          },
-          '3': {
-            question: 'Which genre of fiction do you prefer?',
-            answers: [
-              'Steampunk',
-              'Sci-fi',
-              'Cyberpunk'
-            ],
-            '0': 'Assembler',
-            '1': 'C',
-            '2': 'Go'
-          },
-          '4': {
-            question: 'Which subject would be the first?',
-            answers: [
-              'Math',
-              'Economics',
-              'Statistics',
-              'Informatics',
-              'Physics'
-            ],
-            '0': 'Matlab',
-            '1': 'Java',
-            '2': 'R',
-            '3': 'Python',
-            '4': 'C'
-          }
-        },
-        '2': {
-          question: 'CEO of which company would you like to be?',
-          answers: [
-            'Apple',
-            'Google',
-            'Windows'
-          ],
-          '0': {
-            question: 'Which coffee do you like?',
-            answers: [
-              'Latte',
-              'Сappuccino',
-              'Espresso'
-            ],
-            '0': 'Swift',
-            '1': 'Objective-C',
-            '2': 'Java'
-          },
-          '1': {
-            question: 'Which cuisine do you like?',
-            answers: [
-              'Chinese',
-              'European',
-              'Elvish',
-              'Mexican'
-            ],
-            '0': 'Objective-C',
-            '1': 'Java',
-            '2': 'JS',
-            '3': 'Python'
-          },
-          '2': {
-            question: 'Which cuisine do you like?',
-            answers: [
-              'Chinese',
-              'European',
-              'Elvish',
-              'Mexican'
-            ],
-            '0': 'Objective-C',
-            '1': 'Java',
-            '2': 'JS',
-            '3': 'Python'
-          }
-        }
-      },
+      activePhrase: 0,
       activeAnswer: 0,
       answersStack: [],
       result: ''
@@ -332,7 +54,7 @@ export default {
 
   computed: {
     activeTest() {
-      let result = this.test
+      let result = testData
       this.answersStack.forEach(answer => {
         result = result[answer]
       })
@@ -356,18 +78,22 @@ export default {
       setTimeout(() => {
         clearInterval(timerId)
         this.loading = false
+        this.activePhrase = 0
       }, this.loaderPhrases.length * 1500)
     },
     onKeydown(e) {
       if (this.phase == 0 && e.keyCode == 13) {
         this.loading = true
+        const timerId = setInterval(() => this.loaderBar += '#', 500)
         setTimeout(() => {
-          this.phase = 1
+          clearInterval(timerId)
           this.loading = false
-        }, 1000)
+          this.phase = 1
+        }, 3000)
       } else if (this.phase == 1) {
         if (e.keyCode == 13) {
           this.answersStack.push(this.activeAnswer)
+          this.activeAnswer = 0
         } else if (e.keyCode == 38) {
           this.activeAnswer = this.activeAnswer > 0
           ? this.activeAnswer - 1
@@ -380,6 +106,7 @@ export default {
       } else if (e.keyCode == 13) {
         this.phase = 0
         this.answersStack = []
+        this.loaderBar = ''
       }
     }
   }
@@ -541,7 +268,7 @@ body {
   left: 10%;
   width: 80%;
 }
-.loader-title, .loader-percent {
+.loader-title {
   font-size: 4rem;
   margin: 0 1rem;
 }
---
+++ src/testData.js
@@ -0,0 +1,279 @@
+export default {
+  question: 'What is your purpose of studying?',
+  answers: [
+    'Web development',
+    'Software development',
+    'Mobile app development'
+  ],
+  '0': {
+    question: 'Do you like interacting with people?',
+    answers: [
+      'I\'m good with people',
+      'I hate them all'
+    ],
+    '0': {
+      question: 'Are you a creative person?',
+      answers: [
+        'I\'m a man of beauty',
+        'Functionality is more important for me'
+      ],
+      '0': 'HTML + CSS',
+      '1': 'JS'
+    },
+    '1': {
+      question: 'Are you a workaholic?',
+      answers: [
+        'Personal life is more important for me',
+        'Work is my credo'
+      ],
+      '0': {
+        question: 'Are you conservative or liberal?',
+        answers: [
+          'I love sorting out retro things',
+          'I\'m futurist'
+        ],
+        '0': 'PHP',
+        '1': 'Node.js'
+      },
+      '1': {
+        question: 'What kind kind of music do you prefer?',
+        answers: [
+          'Retrowave',
+          'Power metal',
+          'Alternative',
+          'Pop'
+        ],
+        '0': 'C#',
+        '1': 'Java',
+        '2': 'Ruby',
+        '3': 'Python'
+      }
+    }
+  },
+  '1': {
+    question: 'How do you usually spend your free time?',
+    answers: [
+      'Gaming!',
+      'Reinstalling OS',
+      'Thinking of business ideas',
+      'Building my own C-3PO',
+      'Doing my homework'
+    ],
+    '0': {
+      question: 'Which type of game you play last time?',
+      answers: [
+        'Indie',
+        'AAA',
+        'Text',
+        'MMORPG'
+      ],
+      '0': {
+        question: 'Which styles of visual art do you prefer?',
+        answers: [
+          'Realism',
+          'Surrealism',
+          'Cubism',
+          'Post-modernism',
+          'Avant-garde',
+          'Primitivism'
+        ],
+        '0': 'C++',
+        '1': 'C#',
+        '2': 'Java',
+        '3': 'Python',
+        '4': 'JS',
+        '5': 'Delphi'
+      },
+      '1': {
+        question: 'What is you favourite weapon in games?',
+        answers: [
+          'Bow to shot at someone\'s knee 🏹',
+          'Magic/Biotics/Psyonics',
+          'Knife (no matter what game it is)'
+        ],
+        '0': 'C++',
+        '1': 'C#',
+        '2': 'Java'
+      },
+      '2': {
+        question: 'Which type of literature whould you choose?',
+        answers: [
+          'Fantasy',
+          'Sci-fi',
+          'Manual/Docs/Guide',
+          'Documentary',
+          'Classic',
+          'Novel',
+          'Comics (OMG)'
+        ],
+        '0': 'C++',
+        '1': 'C#',
+        '2': 'Java',
+        '3': 'Python',
+        '4': 'Delphi',
+        '5': 'JS',
+        '6': 'ActionScript'
+      },
+      '3': {
+        question: 'Which fictional race would you like to be?',
+        answers: [
+          'Hafling',
+          'Dwarf',
+          'Klingon',
+          'Borg',
+          'Wookiee',
+          'Azari',
+          'Troll'
+        ],
+        '0': 'C++',
+        '1': 'C#',
+        '2': 'Java',
+        '3': 'Python',
+        '4': 'Delphi',
+        '5': 'JS',
+        '6': 'ActionScript'
+      }
+    },
+    '1': {
+      question: 'Which OS do you reinstall?',
+      answers: [
+        'Windows 😩',
+        'Linux (reboot now)',
+        'MacOS'
+      ],
+      '0': {
+        question: 'Which character would you be in the world of LOTR?',
+        answers: [
+          'J. R. R. Tolkien',
+          'Sauron',
+          'Gandalf',
+          'What is LOTR?',
+          'Treebeard',
+          'Dumbledore'
+        ],
+        '0': 'C',
+        '1': 'C++',
+        '2': 'Java',
+        '3': 'Rust',
+        '4': 'Delphi',
+        '5': 'Visual Basic'
+      },
+      '1': {
+        question: 'Which fiction film do you like?',
+        answers: [
+          'Star Trek (old)',
+          'Star Trek (new)',
+          'Star Wars',
+          'Men in black',
+          'Space Odyssey',
+          'Interstellar'
+        ],
+        '0': 'C',
+        '1': 'C++',
+        '2': 'Java',
+        '3': 'Python',
+        '4': 'Delphi',
+        '5': 'Go'
+      },
+      '2': {
+        question: 'Where would you spend your holidays?',
+        answers: [
+          'Home',
+          'Parent\'s home',
+          'Friend\'s home',
+          'Silicon Valley',
+          'Holidays?'
+        ],
+        '0': 'C',
+        '1': 'C++',
+        '2': 'Objective-C',
+        '3': 'Swift',
+        '4': 'Java'
+      }
+    },
+    '2': {
+      question: 'Which way would you choose to conquer the world?',
+      answers: [
+        'Political',
+        'Economical',
+        'Social',
+        'Military'
+      ],
+      '0': 'C++',
+      '1': 'Java',
+      '2': 'Python',
+      '3': 'Ada'
+    },
+    '3': {
+      question: 'Which genre of fiction do you prefer?',
+      answers: [
+        'Steampunk',
+        'Sci-fi',
+        'Cyberpunk'
+      ],
+      '0': 'Assembler',
+      '1': 'C',
+      '2': 'Go'
+    },
+    '4': {
+      question: 'Which subject would be the first?',
+      answers: [
+        'Math',
+        'Economics',
+        'Statistics',
+        'Informatics',
+        'Physics'
+      ],
+      '0': 'Matlab',
+      '1': 'Java',
+      '2': 'R',
+      '3': 'Python',
+      '4': 'C'
+    }
+  },
+  '2': {
+    question: 'CEO of which company would you like to be?',
+    answers: [
+      'Apple',
+      'Google',
+      'Windows'
+    ],
+    '0': {
+      question: 'Which coffee do you like?',
+      answers: [
+        'Latte',
+        'Сappuccino',
+        'Espresso'
+      ],
+      '0': 'Swift',
+      '1': 'Objective-C',
+      '2': 'Java'
+    },
+    '1': {
+      question: 'Which cuisine do you like?',
+      answers: [
+        'Chinese',
+        'European',
+        'Elvish',
+        'Mexican'
+      ],
+      '0': 'Objective-C',
+      '1': 'Java',
+      '2': 'JS',
+      '3': 'Python'
+    },
+    '2': {
+      question: 'Which cuisine do you like?',
+      answers: [
+        'Chinese',
+        'European',
+        'Elvish',
+        'Mexican'
+      ],
+      '0': 'Objective-C',
+      '1': 'Java',
+      '2': 'JS',
+      '3': 'Python'
+    }
+  }
+}
